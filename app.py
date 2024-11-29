613ee5e1ad58b83b791b35af18c56f5b5e1946e3
NepJag <dylan.riquelme@ug.uchile.cl>
2024-11-29 08:58:41 -0300

    Initial Setup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/dbname'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
